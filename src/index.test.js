17e0384bd42889412966ad8d3c039285f94cefa4
cWilgar <carolinewilgar@gmail.com>
2017-11-14 23:39:14 +0000

    update smoke test!

---
+++ src/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import fetchMock from 'fetch-mock'
+import mockData from './mock_data.json'
+
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+
+const initialState = {
+  photos: null,
+  error: false
+}
+const mockStore = configureMockStore([thunk])
+
+it('renders without crashing', () => {
+  fetchMock.getOnce(
+      /https:\/\/api.flickr.com\/services\/rest\/*/,
+      { body: mockData, headers: { 'content-type': 'application/json' }
+    })
+
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={mockStore(initialState)}>
+      <App />
+    </Provider>, div
+  )
+})
