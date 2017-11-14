8bdb792ea162f56eabb475ad69455aae62a39f0b
cWilgar <carolinewilgar@gmail.com>
2017-11-14 20:49:22 +0000

    lazy load image tiles

--- src/components/PhotoTile/index.js
+++ src/components/PhotoTile/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import './photo-tile.css';
 
 function PhotoTile(props) {
@@ -10,22 +11,23 @@ function PhotoTile(props) {
     const photoTags = photo.tags.replace(/ /g, ', ');
 
     return (
-      <div className="panel-primary panel--phototile">
-        <img src={photo.url_s} alt={photo.title}
-          srcSet={`${photo.url_s} ${photo.width_s}w, ${photo.url_m} ${photo.width_m}w, ${photo.url_l} ${photo.width_l}w`}
-          sizes="(min-width: 1000px) 1000px, 100vw"
-        />
-        <div className="panel-body">
-          <a target="_blank" href={photoLink}>{photo.title}</a> by 
-          _<a target="_blank" href={ownerLink}>{photo.ownername}</a>
-          <p
-            dangerouslySetInnerHTML={{ __html: photo.description._content }} 
+      <LazyLoad height={500} once>
+        <div className="panel-primary panel--phototile">
+          <img src={photo.url_s} alt={photo.title}
+            srcSet={`${photo.url_s} ${photo.width_s}w, ${photo.url_m} ${photo.width_m}w, ${photo.url_l} ${photo.width_l}w`}
+            sizes="(min-width: 1000px) 1000px, 100vw"
           />
-          <p>
-            <b>Tags:</b> {photoTags}
-          </p>
+          <div className="panel-body">
+            <a target="_blank" href={photoLink}>{photo.title}</a> by <a target="_blank" href={ownerLink}>{photo.ownername}</a>
+            <p
+              dangerouslySetInnerHTML={{ __html: photo.description._content }} 
+            />
+            <p>
+              <b>Tags:</b> {photoTags}
+            </p>
+          </div>
         </div>
-      </div>
+      </LazyLoad>
     );
 }
 
