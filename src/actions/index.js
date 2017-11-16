38b3468f98b51d62c2a46501b5459075cfeea76f
cWilgar <carolinewilgar@gmail.com>
2017-11-16 20:01:53 +0000

    add infinite scroll

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,19 @@
 const queryString = require('query-string')
-const request = `https://api.flickr.com/services/rest/?${
-  queryString.stringify({
-    api_key: 'd560b53b95f87253b6ff64c652583031',
-    format: 'json',
-    nojsoncallback: 1,
-    method: 'flickr.photos.search',
-    tags: 'legoland',
-    per_page: 30,
-    extras: 'owner_name,url_s,url_m,url_l,description,tags'
-  })
-}`
 
-export function fetchPhotos () {
+export function fetchPhotos (page) {
+  const request = `https://api.flickr.com/services/rest/?${
+    queryString.stringify({
+      api_key: 'd560b53b95f87253b6ff64c652583031',
+      format: 'json',
+      nojsoncallback: 1,
+      method: 'flickr.photos.search',
+      tags: 'legoland',
+      extras: 'owner_name,url_s,url_m,url_l,description,tags',
+      per_page: 15,
+      page: page
+    })
+  }`
+
   return function (dispatch) {
     fetch(request)
       .then(response => response.json())
--- src/containers/PhotoStreamContainer.js
+++ src/containers/PhotoStreamContainer.js
@@ -6,18 +6,13 @@ import PhotoStream from '../components/PhotoStream'
 const mapStateToProps = state => state
 const mapDispatchToProps = dispatch => {
   return {
-    getPhotoStream: () => {
-      dispatch(actions.fetchPhotos())
+    getPhotoStream: (page) => {
+      dispatch(actions.fetchPhotos(page))
     }
   }
 }
 
 class photoStreamContainer extends React.Component {
-  componentWillMount () {
-    if (!this.props.photos) {
-      this.props.getPhotoStream()
-    }
-  }
   render () {
     return new PhotoStream({...this.props})
   }
